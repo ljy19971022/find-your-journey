f9e536d8d1a5207602668b5808c52aab2d66ce74
Winnie Liu <41314782+ljy19971022@users.noreply.github.com>
2022-04-07 09:57:45 +0800

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -13,7 +13,7 @@ function SearchForm(props) {
     const [evstationsLimit, setEVStationsLimit] = useState(0);
   
     const stateCode = [
-      'AK', 'AZ','AR','CA','CO','CT','DE', 'DC','FL','GA','HI','ID', 'IL','IN',
+      '', 'AK', 'AZ','AR','CA','CO','CT','DE', 'DC','FL','GA','HI','ID', 'IL','IN',
       'IA','KS','KY','LA','ME','MD','MA', 'MI', 'MN', 'MS','MO','MT','NE','NV','NH','NJ',
       'NM','NY','NC','ND','OH','OK', 'OR','PA','RI','SC','SD','TN','TX','UT','VT',
       'VA', 'WA','WV','WI','WY','AS', 'GU','MH', 'FM', 'MP','PW','PR','VI',
